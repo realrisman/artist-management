e0775594fb8dfb06967f276965a70e1ad60687f0
Muhamad Risman <real.risman@gmail.com>
2021-09-26 18:53:37 +0700

    Add security access denied handler

---
+++ src/Sercurity/AccessDeniedHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Security;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface, AuthenticationEntryPointInterface
+{
+
+    protected $json = [
+        'message' => 'Please log in',
+        'error'   => true
+    ];
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+
+        return new JsonResponse($this->json);
+    }
+
+    /*
+    * This method receives the current Request object and the exception by which the exception
+    * listener was triggered.
+    *
+    * The method should return a Response object
+    */
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new JsonResponse($this->json);
+    }
+}
